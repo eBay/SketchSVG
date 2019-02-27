418aff11046b08cedc4c7b4a610f2d057c664bc0
pcanella <pcanella@ebay.com>
2019-02-26 16:26:52 -0800

    Minor changes/cleanup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 const SVGSketch = require('./lib/index');
+const fs = require('fs');
+const colors = require('colors');
 
 if (!process.argv[2]) {
     console.log('please include a valid <filePath> such as sketchsvg /Users/JakePeralta/test.sketch');
     return;
-}
+} else if (process.argv[2] && !fs.existsSync(process.argv[2])) {
+    console.log(colors.bgRed('Uh oh, looks like this file does NOT exist. Maybe there\'s a typo?'));
+    return;
+}               
 
 const instance = new SVGSketch();
 instance.init(process.argv[2]);
