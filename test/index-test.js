9afe8d7ad9b54bfe9d8aa9b6a4ce3e66694a8501
bhaslam <bhaslam@ebay.com>
2019-05-23 10:14:39 -0700

    Update index-test.js

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,12 @@
+/*************************************************************
+Copyright 2019 eBay Inc.
+Author/Developer: Patrick Canella
+
+Use of this source code is governed by an MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+************************************************************/
+
 const cheerio = require('cheerio');
 const sinon = require('sinon');
 const chai = require('chai');
